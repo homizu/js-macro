3711ec5bbed73e8e6f239d2d124d39a775f50647
Ken Wakita <wakita@is.titech.ac.jp>
2012-09-12 22:58:59 +0900

    macro-examples/kw 直下でなくても動作するように修正

--- ex-js/macro-examples/kw/bin/jsx.sh
+++ ex-js/macro-examples/kw/bin/jsx.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 PATH=$HOME/Software/bin:$PATH
-root=`pwd`
+cwd=`pwd`
+root=$cwd
 module=`basename $root`
 while [ `basename $root` != "ex-js" ]; do root=`dirname $root`; done
 
 cd $root
 
-./expand_gpjs.sh macro-examples/kw/$module/$module.js && \
-    chmod 644 macro-examples/kw/$module/converted/$module-expanded.js
+./expand_gpjs.sh $cwd/$module.js &&
+    chmod 644 $cwd/converted/$module-expanded.js
+
+#./expand_gpjs.sh macro-examples/kw/$module/$module.js && \
+#    chmod 644 macro-examples/kw/$module/converted/$module-expanded.js
