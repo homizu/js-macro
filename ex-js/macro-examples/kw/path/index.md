14ba8480f548bde02c8491472313974dee8739f0
Ken Wakita <wakita@is.titech.ac.jp>
2012-09-12 09:29:26 +0900

    Added CSS to the demo.
    new file:   lib/jsx.css
    modified:   path/OMakefile
    modified:   path/index.html
    modified:   path/index.md
    modified:   path/path.js

--- ex-js/macro-examples/kw/path/index.md
+++ ex-js/macro-examples/kw/path/index.md
@@ -1,4 +1,5 @@
 Title: The Path Macro
+CSS: ../lib/jsx.css
 
 -----
 
@@ -8,7 +9,7 @@ Title: The Path Macro
 
 -----
 
-# Code
+# JSX Code
 
 <script src="http://gist-it.sudarmuthu.com/http://github.com/homizu/js-macro/blob/kw-examples/ex-js/macro-examples/kw/path/path.js"></script>
 
--- ex-js/macro-examples/kw/path/path.js
+++ ex-js/macro-examples/kw/path/path.js
@@ -1,6 +1,5 @@
 expression Path {
   expression: line, f, t;
-
   { path [# f -> t : line #] ...
     => [{ from: f, to: t, via: line }, ...] }
 }
@@ -10,11 +9,8 @@ $(function () {
     (Path
       "新宿" -> "東京" : "中央線"
       "東京" -> "京都" : "新幹線").forEach(function (train) {
-          var $tr, $td;
-          $tr = $('<tr>').appendTo($table);
-          $td = $('<td>').appendTo($tr); $td.text(train.via);
-          $td = $('<td>').appendTo($tr); $td.text(train.from);
-          $td = $('<td>').appendTo($tr); $td.text(train.to);
+          var $tr = $('<tr>').appendTo($table);
+          for (k in train) $('<td>').text(train[k]).appendTo($tr);
         });
     });
 
